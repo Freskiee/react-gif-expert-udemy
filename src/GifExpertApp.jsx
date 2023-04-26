4125e1b8cff730d055437680d2220d5dbfc9acd7
Freskiee <freska_96@hotmail.com>
2023-04-26 03:35:50 -0600

    Cambio a matt groening

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AddCategory, GifGrid} from "./components";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["The simpson"]);
+  const [categories, setCategories] = useState(["Matt Groening"]);
 
   const onAddCategory = (newCategory) => {
     if (categories.includes(newCategory)) return;
